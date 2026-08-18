268ed400c7c71a10f5f447f47985f0a2c09dfc0d
Also2004s <Also2004@qq.com>
2026-08-18 15:22:56 +0800

    发布 1.2.6：用于测试应用内更新功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,9 @@ android {
         minSdk = 26
         targetSdk = 34
         // 版本号：每次发布新版本必须 +1，且只能增加不能减少
-        versionCode = 16
+        versionCode = 17
         // 版本名称：对外展示给用户看的版本号
-        versionName = "1.2.5"
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
